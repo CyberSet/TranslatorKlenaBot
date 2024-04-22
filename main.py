d8c87c2cd919774f7e58d96bb91539eec5d421ab
Alexandr <58629032+CyberSet@users.noreply.github.com>
2024-04-22 15:22:08 +0300

    improve usability

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from googletrans import Translator
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Я бот-переводчик с русского на английский. Используй команду /translate для перевода текста.")
 
-# Функция перевода текста с русского на английский
 def translate_to_english(update, context):
     if len(context.args) == 0:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Использование: /translate <текст>")
@@ -15,13 +14,16 @@ def translate_to_english(update, context):
         translated_text = translator.translate(text, src='ru', dest='en').text
         context.bot.send_message(chat_id=update.effective_chat.id, text=translated_text)
 
-# Главная функция
+def remind_usage(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Пожалуйста, используйте команду /translate для перевода текста. Пример: /translate Привет, как дела?")
+
+
 def main():
-    # Замените 'YOUR_TELEGRAM_BOT_TOKEN' на ваш токен бота
     updater = Updater("6909461627:AAEdMB1un71Qebre1MycnkNTB57hFbnrTLQ", use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("translate", translate_to_english))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, remind_usage))
     updater.start_polling()
     updater.idle()
 
